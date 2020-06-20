0cfcdb9223e1b8df4d1010973e9daed46617aec8
adria <adria@DESKTOP-RON13KP>
2020-06-20 19:27:11 -0300

    codigo para trabajar con las credenciales de acceso de mercadolibre

--- application/controllers/Apimeli.php
+++ application/controllers/Apimeli.php
@@ -6,14 +6,22 @@ if( ! ini_get('date.timezone') )
 }
 
 class Apimeli extends CI_Controller {
-    public $token_acces="5114Cirilo1128!";
+    public $token_acces="";
     
     public function __construct(){
         parent::__construct();
         $this->load->model('Apimeli_model');
         $this->load->model('Cliente_model');
+        $this->load->model('Configuracion_model');
+        $base_token=$this->Configuracion_model->get_configuracion(8);
+        $this->token_acces=$base_token["dato"];
     }
-    
+    //se obtine el acces token para hacer las consultas
+    public function get_acces_token() {
+        $base_token=$this->Configuracion_model->get_configuracion(8);
+        echo json_encode($base_token);
+    }
+    // se obtine el listado de las cuentas creadas de mercadolibre de la base de datos
     public function get_api_meli() {
         $token = $this->input->post("token");
         if($token==$this->token_acces){
@@ -28,8 +36,8 @@ class Apimeli extends CI_Controller {
         echo json_encode($listado);
         
     }
-    
-    public function insert_api_meli() {
+    // se insertar por api los clientes que se crean en la app de meli
+    public function insert_cliente_api_meli() {
         $token = $this->input->post("token");
         
         if($token==$this->token_acces){
@@ -56,6 +64,77 @@ class Apimeli extends CI_Controller {
                     $listado= $this->Cliente_model->insertar($razon_social,$telefono);
                 }           
             
+            
+        }else{
+            $listado=array(
+               "code" =>"401",
+               "error" =>"Unauterized=el token no es valido" 
+            );
+        }
+        echo json_encode($listado);
+    }
+    // se insertan las credenciales creadas desde la app y se persisten los datos creados
+    public function insert_api_meli() {
+        $token = $this->input->post("token");
+        
+        if($token==$this->token_acces){
+            
+            $id_cliente=$this->input->post("id_cliente");
+            $user_id=$this->input->post("user_id");
+            $code=$this->input->post("code");
+            $access_token=$this->input->post("access_token");
+            $expires_in=$this->input->post("expires_in");
+            $refresh_token=$this->input->post("refresh_token");
+            $endpoint=$this->input->post("endpoint");
+            
+            $this->load->helper(array('form', 'url'));
+
+                $this->load->library('form_validation');
+
+                $this->form_validation->set_rules('user_id', 'user_id', 'required');
+                $this->form_validation->set_rules('access_token', 'access_token', 'required');
+                $this->form_validation->set_rules('expires_in', 'expires_in', 'required');
+                $this->form_validation->set_rules('refresh_token', 'refresh_token', 'required');
+                
+                if ($this->form_validation->run() == FALSE)
+                {
+                    $listado=array(
+                        "code" =>"400",
+                        "error" =>"campos incorrectos" 
+                     ); 
+                }
+                else
+                {
+                    $listado= $this->Apimeli_model->insertar($id_cliente,$user_id,$code,$access_token,$expires_in,$refresh_token,$endpoint);
+                }           
+            
+            
+        }else{
+            $listado=array(
+               "code" =>"401",
+               "error" =>"Unauterized=el token no es valido" 
+            );
+        }
+        echo json_encode($listado);
+    }
+    // para verificar si existe el usser id en la base de datos y luego verificar si se inserta o se actualizar los tokens
+    public function existe_usuario() {
+        $token = $this->input->post("token");
+        $user = $this->input->post("usser_id");
+        
+        if($token==$this->token_acces){
+            $listado= $this->Apimeli_model->get_api_meli_user($user);
+            if(!empty($listado)){
+                $listado=array(
+                    "code" =>"200",
+                    "existe" =>true
+                 ); 
+            }else{
+                $listado=array(
+                    "code" =>"200",
+                    "existe" =>false
+                 );
+            }
             
         }else{
             $listado=array(
--- application/models/Apimeli_model.php
+++ application/models/Apimeli_model.php
@@ -18,12 +18,44 @@ class Apimeli_model extends CI_Model {
     }
     
     public function get_api_meli(){
-        $sql="SELECT * FROM api_meli";
+        $sql="SELECT * FROM api_meli where estado='activo'";
         
         $query = $this->db->query($sql);
         
         return $query->result_array();
 		
+    }
+    
+    public function get_api_meli_user($user){
+        $sql="SELECT * FROM api_meli where user_id=$user";
+        
+        $query = $this->db->query($sql);
+        
+        return $query->row_array();
+		
+    }
+    
+    public function insertar($id_cliente,$user_id,$code,$access_token,$expires_in,$refresh_token,$endpoint) {
+
+        $data = array(
+            
+            'id_cliente'=>$id_cliente,
+            'user_id'=>$user_id,
+            'code'=>$code,
+            'access_token'=>$access_token,
+            'expires_in'=>$expires_in,
+            'refresh_token'=>$refresh_token,
+            'endpoint'=>$endpoint,
+            'estado'=>'activo',
+        );
+
+        $accion=$this->db->insert('api_meli', $data);
+        $insertId = $this->db->insert_id();
+        
+        return array(
+            'exito'=>$accion,
+            'id'=>$insertId
+        );
     }
     
 }
